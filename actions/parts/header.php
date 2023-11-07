acdf3d942b3bf173860f1e284f856242301d0991
Paweł Ługowski <pawel.lugowski02@gmail.com>
2023-11-07 17:59:05 +0100

    Moving css files to one folder

--- actions/parts/header.php
+++ actions/parts/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../style.css">
-    <link id="stylesheet_dark" rel="stylesheet" href="../style_dark.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <link id="stylesheet_dark" rel="stylesheet" href="../style/style_dark.css">
     <script>
         function confirmDelete() {
             return confirm("Czy na pewno chcesz usunąć?");
